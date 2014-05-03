5a55bb04033b85925220ef4a46ccb03c1759c2a6
Johan Lövdahl <johan.lovdahl@uxvibe.com>
2014-05-03 18:20:53 +0200

    Add range test with individual transactions
    Insertion of data and each get request is run in
    separate transactions.

--- test/fdb_test.erl
+++ test/fdb_test.erl
@@ -29,7 +29,26 @@ transaction_test() ->
 range_test() ->
   DB = fdb:init_and_open(?SOLIB),
   fdb:transact(DB, fun(Tx) ->
-    [ok = fdb:set(Tx, I, I) || I <- lists:seq(1, 9)],
+                       [ok = fdb:set(Tx, I, I) || I <- lists:seq(1, 9)]
+                   end),
+  ?assertEqual([{2, 2}, {3, 3},{4, 4}], fdb:get(DB, #select{gte = 2, lte = 4})),
+  ?assertEqual([{2, 2}, {3, 3}], fdb:get(DB, #select{ gte = 2, lte =3})),
+  ?assertEqual([{2, 2}, {3, 3}], fdb:get(DB, #select{ gt = 1, lt = 4})),
+  ?assertEqual([{8, 8}, {9, 9}], fdb:get(DB, #select{ gt = 7, lt = 10})),
+  ?assertEqual([{1, 1}, {2, 2}], fdb:get(DB, #select{ gt = 0, lt = 3})),
+  ?assertEqual([{2, 2}, {3, 3}], fdb:get_range(DB, 2, 4)),
+  ?assertEqual([{1, 1}, {2, 2}], fdb:get(DB, #select{ gt=0, lt = 3})),
+  ?assertEqual([{1, 1}, {2, 2}], fdb:get(DB, #select{ gt=0, lt = 3})),
+  ?assertEqual([{8, 8}, {9, 9}], fdb:get(DB, #select{ gt=7})),
+  ?assertEqual([{8, 8}, {9, 9}], fdb:get(DB, #select{ gte = 8})),
+  fdb:clear_range(DB, 4, 8),
+  ?assertEqual([{3, 3}, {8, 8}], fdb:get(DB, #select{ gt = 2, lt = 9})),
+  fdb:clear_range(DB, 1, 9).
+
+range_single_transaction_test() ->
+  DB = fdb:init_and_open(?SOLIB),
+  fdb:transact(DB, fun(Tx) ->
+   [ok = fdb:set(Tx, I, I) || I <- lists:seq(1, 9)],
     ?assertEqual([{2, 2}, {3, 3},{4, 4}], fdb:get(Tx, #select{gte = 2, lte = 4})),
     ?assertEqual([{2, 2}, {3, 3}], fdb:get(Tx, #select{ gte = 2, lte =3})),
     ?assertEqual([{2, 2}, {3, 3}], fdb:get(Tx, #select{ gt = 1, lt = 4})),
@@ -42,5 +61,7 @@ range_test() ->
     ?assertEqual([{8, 8}, {9, 9}], fdb:get(Tx, #select{ gte = 8})),
     fdb:clear_range(Tx, 4, 8),
     ?assertEqual([{3, 3}, {8, 8}], fdb:get(Tx, #select{ gt = 2, lt = 9}))
-  end).
+                   end),
+  fdb:clear_range(DB, 1, 9).
+  
 
